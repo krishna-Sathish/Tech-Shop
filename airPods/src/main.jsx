16331e6db95255ad0882c93bb4687744466ed040
krishna-Sathish <romansathish3337@gmail.com>
2024-11-29 14:35:31 +0530

    Changes modified

--- airPods/src/main.jsx
+++ airPods/src/main.jsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/React-Project'>
+    <BrowserRouter basename='/Tech-Shop'>
       <App />
     </BrowserRouter>
   </StrictMode>
--- airPods/vite.config.js
+++ airPods/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/React-Project",
+  base:"/Tech-Shop/",
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
